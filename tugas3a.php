f022a7defae76055c155bf781c7e9c2926468f02
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-07 12:20:25 +0700

    Add files via upload

---
+++ tugas3a.php
@@ -0,0 +1,10 @@
+<?php
+	$jarak = 360;
+	$kecepatan = 90;
+	$waktu = ($jarak/60)*$kecepatan;
+
+	echo "Jarak Kota A - Z = $jarak km<br>
+	Kecepatan motor = $kecepatan km/jam<br>
+	Waktu Tempuh = ? <br>
+	Jawaban : $waktu menit";
+?>
